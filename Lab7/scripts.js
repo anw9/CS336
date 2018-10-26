b53be04ae61dbae292596127f889236854fe7e6a
anw9 <32888892+anw9@users.noreply.github.com>
2018-10-26 18:02:22 -0400

    deleting excess files

--- Lab7/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-//on button push a new div with italic text will be printed bellow the button
-$('#btt').on('click', function () {
- $("<em>", {html: "No data to display yet..."}).appendTo("body");
-});
-});
