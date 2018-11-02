a7b7d3d8265d9eabfc802301843c46fdded7d584
anw9 <32888892+anw9@users.noreply.github.com>
2018-11-02 16:02:12 -0400

    Adding lab9

---
+++ lab9/app/scripts/index.js
@@ -0,0 +1,16 @@
+
+//Arie Williams
+//Oct. 22 2018
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+import $ from 'jquery';
+
+import CommentBox from './commentBox';
+import '../css/base.css';
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
