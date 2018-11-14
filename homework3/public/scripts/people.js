aa5eca781627f7343edfd9a83465c49e1c99fc21
anw9 <32888892+anw9@users.noreply.github.com>
2018-11-13 19:22:15 -0500

    Adding hw3

---
+++ homework3/public/scripts/people.js
@@ -0,0 +1,134 @@
+var PeopleBox = React.createClass({
+    loadPeopleFromServer: function() {
+        $.ajax({
+          url: this.props.url,
+          dataType: 'json',
+          cache: false,
+          success: function(data) {
+            this.setState({data: data});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+      },
+      handlePersonSubmit: function(person) {
+        var people = this.state.data;
+        var newPeople = people.concat([person]);
+        this.setState({data: newPeople});
+        $.ajax({
+          url: this.props.url,
+          dataType: 'json',
+          type: 'POST',
+          data: person,
+          success: function(data) {
+            this.setState({data: data});
+          }.bind(this),
+          error: function(xhr, status, err) {
+            this.setState({data: people});
+            console.error(this.props.url, status, err.toString());
+          }.bind(this)
+        });
+      },
+      getInitialState: function() {
+        return {data: []};
+      },
+      componentDidMount: function() {
+        this.loadPeopleFromServer();
+        setInterval(this.loadPeopleFromServer, this.props.pollInterval);
+      },
+      render: function() {
+        return (
+          <div className="peopleBox">
+            <h1>People</h1>
+            <PeopleList data={this.state.data} />
+            <PersonForm onPersonSubmit={this.handlePersonSubmit} />
+          </div>
+        );
+      }
+});
+
+var PersonForm = React.createClass({
+    getInitialState: function() {
+        return {firstName: '', lastName: '', workID: '', startDate: ''};
+      },
+      handleFirstNameChange: function(e) {
+        this.setState({firstName: e.target.value});
+      },
+      handleLastNameChange: function(e) {
+        this.setState({lastName: e.target.value});
+      },
+      handleWorkIdChange: function(e) {
+        this.setState({workID: e.target.value});
+      },
+      handleStartDateChange: function(e) {
+        this.setState({startDate: e.target.value});
+      },
+      handleSubmit: function(e) {
+        e.preventDefault();
+        var firstName = this.state.firstName.trim();
+        var lastName = this.state.lastName.trim();
+        var workID = this.state.workID.trim();
+        var startDate = this.state.startDate.trim();
+        if (!firstName || !lastName || !workID || !startDate) {
+          return;
+        }
+        this.props.onPersonSubmit({firstName: firstName, lastName: lastName, workID: workID, startDate: startDate});
+        this.setState({firstName: '', lastName: '', workID: '', startDate: ''});
+      },
+      render: function() {
+        return (
+          <form className="personForm" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Your first name"
+              value={this.state.firstName}
+              onChange={this.handleFirstNameChange}
+            />
+            <input
+              type="text"
+              placeholder="Your last name"
+              value={this.state.lastName}
+              onChange={this.handleLastNameChange}
+            />
+            <input
+              type="text"
+              placeholder="Your Work ID"
+              value={this.state.workID}
+              onChange={this.handleWorkIdChange}
+            />
+            <input
+              type="text"
+              placeholder="Your start date"
+              value={this.state.startDate}
+              onChange={this.handleStartDateChange}
+            />
+            <input type="submit" value="Post" />
+          </form>
+        );
+      }
+});
+
+var Person = React.createClass({
+    render: function() {
+        return (
+            <div className="person">
+            <h2 className="name">
+                {this.props.firstName} {this.props.lastName}
+            </h2>
+
+            <p className="workID">
+                Login ID: {this.props.workID}
+            </p>
+            <p className="startDate">
+                Start Date: {this.props.startDate}
+            </p>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <PeopleBox url="/people" pollInterval={2000} />,
+    document.getElementById('content')
+);
---
+++ homework3/server.js
@@ -0,0 +1,142 @@
+/*
+Arie Williams
+Oct 22 2018
+*/
+var fs = require('fs');
+var path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
+assert = require('assert');
+const bodyParser = require('body-parser');
+//Adding Mongo elements
+var db;
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*Defines the comment file as a variable so we can read from it*/
+var COMMENTS_FILE = path.join(__dirname, 'names.json');
+//Serving from the public folder
+app.use(express.static("public"));
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+/* People Getter, just returns all the People*/
+app.get('/people', function(req, rec) {
+  //Chaning to access mongoDB
+  db.collection('people').find().toArray(function (err, result){
+    assert.equal(null, err);
+
+    rec.json(result);
+  });
+});
+
+/*This is the app.post function for the ajax database post request*/
+app.post('/people', function(req, res){
+  db.collection('people').insertOne(
+        {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            workId: req.body.workID,
+            startDate: req.body.stDate
+
+        },
+        function(err, r) {
+          assert.equal(null, err);
+        }
+    );
+});
+
+/*This is getter that pulls the json data requested based on the id pf the perosn being req*/
+/* Getting the people with the specific ID's*/
+app.get('/person/:id', function(req, rec) {
+  db.collection('people').find().toArray(function (err, result) {
+         assert.equal(null, err);
+
+         for (const person of result) {
+             if (person["workID"] == req.params.id) {
+                 rec.json(person);
+                 return;
+             }
+         }
+     });
+});
+
+// This is the updating of a person, we use the put command to do updates in js
+// curl -X PUT -H "Content-Type: application/json" -d '{"firstName":"Jonah","lastName":"Oji","workId":"jOjo","startDate":"2010/04/12"}' http://localhost:3000/person/jOji
+app.put('/person/:id', function(req, res) {
+  db.collection('people').updateOne({workID: req.params.id},
+    {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      workId: req.body.workID,
+      startDate: req.body.stDate
+    },
+    function(err, result) {
+      assert.equal(null, err);
+      res.json("Updated data for ID: " + req.params.id);
+      return;
+    }
+  );
+});
+
+
+// This the delete method it is removing a person from the given database
+// curl -X DELETE http://localhost:3000/person/jOji
+app.delete('/person/:id', function(req, res) {
+  db.collection('people').deleteOne({workID: req.params.id}, function(err, result) {
+       assert.equal(null, err);
+       res.json("Deleted person with ID: " + req.params.id);
+       return;
+   });
+  });
+
+/* Returning the people with the given ID's name */
+app.get('/person/:id/name', function (req, rec) {
+
+  db.collection('people').find().toArray(function (err, result) {
+    assert.equal(null, err);
+    for (const person of result) {
+      if (person["workID"] == req.params.id) {
+        rec.json('${person.firstName} ${person.lastName}');
+        return;
+      }
+    }
+  });
+
+});
+
+/*Retrieving the date information for number of years a person is in a company*/
+app.get('/person/:id/years', function (req, rec) {
+
+db.collection('people').find().toArray(function (err, result) {
+  assert.equal(null, err);
+  for (const person of result) {
+    if (person["workID"] == req.params.id) {
+      var today = new Date();
+      var startDate = new Date(person.startDate);
+      var age = today.getFullYear() - startDate.getFullYear();
+
+      rec.send('Years in organization: ${age}');
+      return;
+    }
+  }
+});
+
+});
+
+/* Connecting to Mongo servers for the data*/
+MongoClient.connect('mongodb://cs336:' + process.env.MONGO_PASSWORD + '@ds045087.mlab.com:45087/cs336', function (err, client) {
+  if (err) throw err
+
+  db = client;
+
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+  db.collection('people').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  });
+});
